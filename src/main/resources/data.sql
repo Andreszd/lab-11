23eb0da21dc47ee09c5aafbf6fc1a9950647c04c
joseandre <contretaa@gmail.com>
2021-09-08 23:14:56 -0400

    add: some queries in data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO CUSTOMER (id, birthday, firstname, lastname, email)
+VALUES (1, '1999-11-13', 'jose', 'jimenez', 'contretaa@gmail.com');
+INSERT INTO CUSTOMER (id, birthday, firstname, lastname, email)
+VALUES (2, '1999-09-08', 'Melany', 'brouset','mel@gmail.com');
